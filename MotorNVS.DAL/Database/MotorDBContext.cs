96887b9cab48927cfe6a46edc97de1abb2ec8487
Nicklas Valintin Sams <nicklasvsams@gmail.com>
2022-03-10 18:00:31 +0100

    Basic setup to start creating functionality.

--- MotorNVS.DAL/Database/MotorDBContext.cs
+++ MotorNVS.DAL/Database/MotorDBContext.cs
@@ -8,17 +8,5 @@ namespace MotorNVS.DAL.Database
         public MotorDBContext() { }
 
         public MotorDBContext(DbContextOptions<MotorDBContext> contextOptions) : base(contextOptions) { }
-
-        DbSet<ZipCode>? ZipCode { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<ZipCode>().HasData(
-                new ZipCode()
-                {
-                    Id = 1,
-                    ZipcodeNo = "2450"
-                });
-        }
     }
 }
