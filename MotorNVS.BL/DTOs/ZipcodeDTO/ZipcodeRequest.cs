0954a4a6d99ae62f57104e10a5e2a5a5aa2701a2
Nicklas Valintin Sams <nicklasvsams@gmail.com>
2022-03-13 17:08:05 +0100

    Zipcode repository, service and DTOs implemented

---
+++ MotorNVS.BL/DTOs/ZipcodeDTO/ZipcodeRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotorNVS.BL.DTOs.ZipcodeDTO
+{
+    public class ZipcodeRequest
+    {
+        [Required]
+        [StringLength(20, ErrorMessage = "Zipcode can not be longer than 20 characters long")]
+        public string ZipcodeNo { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "City can not be longer than 50 characters long")]
+        public string City { get; set; }
+    }
+}
---
+++ MotorNVS.BL/DTOs/ZipcodeDTO/ZipcodeResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MotorNVS.BL.DTOs.ZipcodeDTO
+{
+    public class ZipcodeResponse
+    {
+        public int Id { get; set; }
+        public string ZipcodeNo { get; set; }
+        public string City { get; set; }
+    }
+}
