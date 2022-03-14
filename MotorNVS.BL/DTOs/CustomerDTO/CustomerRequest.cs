42c0363a3a07c76b4bdecb4074beeb3ba4eb5ee4
nicklasvsams <nicklasvsams@gmail.com>
2022-03-14 10:00:09 +0100

    Customer repository, service and DTOs implemented

---
+++ MotorNVS.BL/DTOs/CustomerDTO/CustomerRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotorNVS.BL.DTOs.CustomerDTO
+{
+    public class CustomerRequest
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "First name can not be longer than 50 characters long")]
+        public string FirstName { get; set; }
+        [StringLength(50, ErrorMessage = "Last name can not be longer than 50 characters long")]
+        public string? LastName { get; set; }
+        [Required]
+        public DateTime CreateDate { get; set; }
+        [Required]
+        public int AddressId { get; set; }
+    }
+}
---
+++ MotorNVS.DAL/Repositories/CustomerRepository.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MotorNVS.DAL.Database;
+using MotorNVS.DAL.Database.Entities;
+
+namespace MotorNVS.DAL.Repositories
+{
+    public interface ICustomerRepository
+    {
+        Task<List<Customer>> SelectAllCustomers();
+        Task<Customer> SelectCustomerById(int customerId);
+        Task<Customer> InsertNewCustomer(Customer customer);
+        Task<Customer> DeleteCustomerById(int customerId);
+        Task<Customer> UpdateCustomerById(int customerId, Customer customer);
+    }
+
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly MotorDBContext _dBContext;
+
+        public CustomerRepository(MotorDBContext dBContext)
+        {
+            _dBContext = dBContext;
+        }
+
+        public async Task<Customer> DeleteCustomerById(int customerId)
+        {
+            Customer customerToDelete = await _dBContext
+                .Customer
+                .FirstOrDefaultAsync(x => x.Id == customerId);
+
+            if(customerToDelete != null)
+            {
+                _dBContext.Remove(customerToDelete);
+                await _dBContext.SaveChangesAsync();
+            }
+
+            return customerToDelete;
+        }
+
+        public async Task<Customer> InsertNewCustomer(Customer customer)
+        {
+            await _dBContext.AddAsync(customer);
+            await _dBContext.SaveChangesAsync();
+
+            return customer;
+        }
+
+        public async Task<List<Customer>> SelectAllCustomers()
+        {
+            return await _dBContext
+                .Customer
+                .Include("Address")
+                .Include("Zipcode")
+                .ToListAsync();
+        }
+
+        public async Task<Customer> SelectCustomerById(int customerId)
+        {
+            return await _dBContext
+                .Customer
+                .Include("Address")
+                .Include("Zipcode")
+                .FirstOrDefaultAsync(x => x.Id == customerId);
+        }
+
+        public async Task<Customer> UpdateCustomerById(int customerId, Customer customer)
+        {
+            Customer customerToUpdate = await _dBContext
+                .Customer
+                .FirstOrDefaultAsync(x => x.Id == customerId);
+
+            if(customerToUpdate != null)
+            {
+                customerToUpdate.FirstName = customer.FirstName;
+                customerToUpdate.LastName = customer.LastName;
+                customerToUpdate.CreateDate = customer.CreateDate;
+                customerToUpdate.AddressId = customer.AddressId;
+            }
+
+            return customerToUpdate;
+        }
+    }
+}
