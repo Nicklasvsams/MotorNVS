131dc0fa98b17391adb1dd4eceb9726661ba2584
nicklasvsams <nicklasvsams@gmail.com>
2022-03-14 09:15:11 +0100

    Address repository, service and DTOs implemented

---
+++ MotorNVS.BL/DTOs/AddressDTO/AddressResponse.cs
@@ -0,0 +1,14 @@
+﻿using MotorNVS.BL.DTOs.ZipcodeDTO;
+
+namespace MotorNVS.BL.DTOs.AddressDTO
+{
+    public class AddressResponse
+    {
+        public int Id { get; set; }
+        public string StreetAndNo { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int ZipcodeId { get; set; }
+
+        public ZipcodeResponse ZipcodeResponse { get; set; }
+    }
+}
